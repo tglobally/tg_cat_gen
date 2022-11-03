4c667285cb1450e441c68a61e5ffb291a777cc74
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-03 18:14:36 -0500

    v0.25.0 Se integra acciones en js para conf de empleados

--- js/nom_conf_empleado/alta.js
+++ js/nom_conf_empleado/alta.js
@@ -1,32 +1,23 @@
-let url = getAbsolutePath();
-let session_id = getParameterByName('session_id');
-
 let sl_nom_empleado = $("#em_empleado_id");
-let sl_em_cuenta_bancaria_id = $("#em_cuenta_bancaria_id");
+let sl_em_cuenta_bancaria = $("#em_cuenta_bancaria_id");
 
 sl_nom_empleado.change(function(){
     let selected = $(this).find('option:selected');
     let em_empleado_id = $(this).val();
-    let url = "index.php?seccion=em_cuenta_bancaria&ws=1&accion=get_cuentas_bancarias&em_empleado_id="+em_empleado_id+"&session_id="+session_id;
 
-    getData(url,(data) => {
-        sl_em_cuenta_bancaria_id.empty();
-        integra_new_option("#em_cuenta_bancaria_id",'Seleccione una cuenta bancaria','');
+    let url = get_url("em_cuenta_bancaria","get_cuentas_bancarias", {em_empleado_id: em_empleado_id});
+
+    get_data(url, function (data) {
+        sl_em_cuenta_bancaria.empty();
+
+        integra_new_option(sl_em_cuenta_bancaria,'Seleccione una cuenta','-1');
 
         $.each(data.registros, function( index, em_cuenta_bancaria ) {
-            integra_new_option("#em_cuenta_bancaria_id",em_cuenta_bancaria.bn_banco_descripcion_select+' '+em_cuenta_bancaria.em_cuenta_bancaria_num_cuenta,em_cuenta_bancaria.em_cuenta_bancaria_id);
+            integra_new_option(sl_em_cuenta_bancaria,em_cuenta_bancaria.bn_banco_descripcion_select+' '+
+                em_cuenta_bancaria.em_cuenta_bancaria_num_cuenta,em_cuenta_bancaria.em_cuenta_bancaria_id);
         });
 
-        sl_em_cuenta_bancaria_id.selectpicker('refresh');
+        sl_em_cuenta_bancaria.selectpicker('refresh');
     });
 });
 
-let getData = async (url, acciones) => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => acciones(data))
-        .catch(err => {
-            alert('Error al ejecutar');
-            console.error("ERROR: ", err.message)
-        });
-}
