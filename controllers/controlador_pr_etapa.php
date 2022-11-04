3902aad5c2404e7226bd524214a0267f10f822c3
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-04 11:06:18 -0500

    v0.26.0 Se ajusta catalogo pr_etapa

--- controllers/controlador_pr_etapa.php
+++ controllers/controlador_pr_etapa.php
@@ -21,68 +21,33 @@ class controlador_pr_etapa extends \gamboamartin\proceso\controllers\controlador
 
         $html_base = new html();
         parent::__construct( link: $link, html: $html_base);
-        $this->titulo_lista = 'Configuracion Factura';
+        $this->titulo_lista = 'Etapa';
 
-        $keys_row_lista = $this->keys_rows_lista();
-        if(errores::$error){
-            $error = $this->errores->error(mensaje: 'Error al generar keys de lista',data:  $keys_row_lista);
-            print_r($error);
-            exit;
-        }
+        $this->sidebar['lista']['titulo'] = "Etapa";
+        $this->sidebar['lista']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_seccion_active: true,
+                menu_lateral_active: true));
 
-        $this->keys_row_lista = $keys_row_lista;
-        $this->total_items_sections = 1;
+        $this->sidebar['alta']['titulo'] = "Alta Etapa";
+        $this->sidebar['alta']['stepper_active'] = true;
+        $this->sidebar['alta']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_lateral_active: true));
 
-        $this->actions_number['lista']['item'] = 1;
-        $this->actions_number['lista']['etiqueta'] = 'Configuracion de factura';
-
-        $this->actions_number['alta']['item'] = 1;
-        $this->actions_number['alta']['etiqueta'] = 'Configuracion de factura';
-
-
-        $this->number_active = 1;
-
-        if(isset($this->actions_number[$this->accion])){
-            $this->number_active = $this->actions_number[$this->accion]['item'];
-        }
-
-        $menu_lateral = (new menu_lateral())->number_head(number_active: $this->number_active);
-        if(errores::$error){
-            $error = $this->errores->error(mensaje: 'Error al integrar include', data: $menu_lateral);
-            print_r($error);
-            exit;
-        }
-        $this->menu_lateral = $menu_lateral;
+        $this->sidebar['modifica']['titulo'] = "Modifica Etapa";
+        $this->sidebar['modifica']['stepper_active'] = true;
+        $this->sidebar['modifica']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_alta,menu_lateral_active: true));
     }
 
-    private function keys_rows_lista(): array
+    public function menu_item(string $menu_item_titulo, string $link, bool $menu_seccion_active = false,bool $menu_lateral_active = false): array
     {
+        $menu_item = array();
+        $menu_item['menu_item'] = $menu_item_titulo;
+        $menu_item['menu_seccion_active'] = $menu_seccion_active;
+        $menu_item['link'] = $link;
+        $menu_item['menu_lateral_active'] = $menu_lateral_active;
 
-        $keys_row_lista = array();
-
-        $keys = array('pr_etapa_id','pr_etapa_codigo','pr_etapa_descripcion','pr_etapa_descripcion_select');
-
-        foreach ($keys as $campo){
-            $keys_row_lista = $this->key_row_lista_init(campo: $campo, keys_row_lista: $keys_row_lista);
-            if(errores::$error){
-                return $this->errores->error(mensaje: 'Error al inicializar key',data: $keys_row_lista);
-            }
-        }
-
-        return $keys_row_lista;
-    }
-
-    private function key_row_lista_init(string $campo, array $keys_row_lista): array
-    {
-        $data = new stdClass();
-        $data->campo = $campo;
-
-        $campo = str_replace(array('pr_etapa', '_'), array('', ' '), $campo);
-        $campo = ucfirst(strtolower($campo));
-
-        $data->name_lista = $campo;
-        $keys_row_lista[]= $data;
-        return $keys_row_lista;
+        return $menu_item;
     }
 
 
